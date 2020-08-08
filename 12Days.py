f30dadaf83f0de676596591e60e94eeedae6bf50
jmorton89 <joel.morton33@gmail.com>
2020-08-08 16:51:59 +0900

    Create 12Days.py

---
+++ 12Days.py
@@ -0,0 +1,9 @@
+d={"Twelth":"Twelve Drummers Druming,","Eleventh":"Eleven Pipers Piping,","Tenth":"Ten Lords-a-Leaping,","Ninth":"Nine Ladies Dancing,"
+    ,"Eigth":"Eight Maids-a-Milking,","Seventh":"Seven Swans-a-Swimming,","Sixth":"Six Geese-a-Laying,","Fifth":"Five Gold Rings,"
+    ,"Fourth":"Four Calling Birds,","Third":"Three French Hens,","Second":"Two Turtle Doves, and","First":"A Partridge in a Pear Tree."}
+for key, value in d.items():
+    print("\nOn the {} day of Christmas\nMy true love sent to me\n{}".format(key, value))
+
+#L=(["\nTwelve Drummers Druming,","\nEleven Pipers Piping,","\nTen Lords-a-Leaping,","\nNine Ladies Dancing,","\nEight Maids-a-Milking,","\nSeven Swans-a-Swimming,","\nSix Geese-a-Laying,","\nFive Gold Rings,","\nFour Calling Birds,","\nThree French Hens,","\nTwo Turtle Doves, and","\nA Partridge in a Pear Tree."])
+#print(x+L[11]+x+L[10]+L[11]+x+L[9]+L[10]+L[11]+x+L[8]+L[9]+L[10]+L[11])
+#print(list(enumerate(L,1)))
