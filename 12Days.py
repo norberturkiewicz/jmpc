d40218c4da8773ac1f36e6e3cad5db21eb35958f
jmorton89 <joel.morton33@gmail.com>
2020-08-27 17:40:04 +0900

    Update 12Days.py

--- 12Days.py
+++ 12Days.py
@@ -7,7 +7,6 @@
 
 #L=(["\nTwelve Drummers Drumming,","\nEleven Pipers Piping,","\nTen Lords-a-Leaping,","\nNine Ladies Dancing,","\nEight Maids-a-Milking,","\nSeven Swans-a-Swimming,","\nSix Geese-a-Laying,","\nFive Gold Rings,","\nFour Calling Birds,","\nThree French Hens,","\nTwo Turtle Doves, and","\nA Partridge in a Pear Tree."])
 #print(x+L[11]+x+L[10]+L[11]+x+L[9]+L[10]+L[11]+x+L[8]+L[9]+L[10]+L[11])
-# 
 
 #d={1:"First",2:"Second",3:"Third",4:"Fourth",5:"Fifth",6:"Sixth",7:"Seventh",8:"Eighth",9:"Ninth",10:"Tenth",11:"Eleventh",12:"Twelfth"}
 #l=["A Partridge in a Pear Tree.","Two Turtle Doves, and","Three French Hens,","Four Calling Birds,","Five Gold Rings,","Six Geese-a-Laying,","Seven Swans-a-Swimming,","Eight Maids-a-Milking,","Nine Ladies Dancing,","Ten Lords-a-Leaping,","Eleven Pipers Piping,","Twelve Drummers Drumming,"]
@@ -16,10 +15,9 @@
 # x=d.get(i+1)
 # y=l[i]
 # print("\nOn the {} day of Christmas\nMy true love sent to me\n{}".format(x,y))
-# if i>0:
-#  z=reversed(range(0,i))
-#  for i in z:
-#   print(l[i])
+# z=reversed(range(0,i))
+# for i in z:
+#  print(l[i])
 
 days = {
     1: "First",
@@ -49,15 +47,26 @@ words = [
     "Eleven Pipers Piping,",
     "Twelve Drummers Drumming,"
 ]
+
+# iterate for each day of xmas.  There are 12 days.
+# python loops are index origain 0. So the "i" will be
+# 0 to 11 not 1 to 12.
 for i in range(12):    
+    # for i, get spelled out day word by the day number
+    # i correlates to day number but we must add 1, beacuse
+    # the loop starts at 0. ^^
+    # .get returns value by key.
     x= days.get(i+1)
+    # words is a list thats ordered in sync with day range.
+    # you can get words value by indexing directly into the list
     y= words[i]
+
+    # print phrase for current day.
     print("\nOn the {} day of Christmas\nMy true love sent to me\n{}".format(x,y))
-    # printing the correct day and gift in the correct order ^^^^
-    if i>0:
-        # while i is being iterated over range 1-12 which is over 0
-        z=reversed(range(0,i))
-        # this range inverts the order from the current range value to the value of i
-        for i in z:
-            # this iterates oer each of the items in that above range
-            print(words[i])
+
+    # we must print each previous gift up to this day.
+    z=reversed(range(0,i))
+    # this range inverts the order from the current range value to the value of i
+    for i in z:
+        # this iterates oer each of the items in that above range
+        print(words[i])
